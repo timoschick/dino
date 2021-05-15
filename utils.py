80c4b02cdde87f31ee82d44f3a349500efd13249
Timo Schick <schickt93@gmail.com>
2021-05-15 16:01:23 +0200

    Add license statement

--- utils.py
+++ utils.py
@@ -1,7 +1,23 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains some utility functions.
+"""
+
 import csv
 import json
 import random
-from typing import List, Optional
+from typing import List, Optional, Any
 
 import numpy as np
 import torch
@@ -73,7 +89,7 @@ def read_sts_inputs(path: str) -> List[str]:
 class DatasetEntry:
     """This class represents a dataset entry for text (pair) classification"""
 
-    def __init__(self, text_a: str, text_b: Optional[str], label: str):
+    def __init__(self, text_a: str, text_b: Optional[str], label: Any):
         self.text_a = text_a
         self.text_b = text_b
         self.label = label
